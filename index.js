6099b2cb7547a497fe8d18f6462112ac9254eb51
monique <moniquefernanda109@gmail.com>
2024-10-23 19:54:23 -0300

    teste

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const app = express();
 function PaginaInicial(requisicao, resposta){
     resposta.send('<h1>Seja bem vindo</h1>');
 }
+function tabuada(requisicao, resposta){
+    let resultado = '';
+    for (let i = 0; i <= sequencia; i++) {
+        resultado += `<p>${numero} x ${i} = ${numero * i}</p>`;
+    }
+    return resultado;
+}
 app.get("/",PaginaInicial);
+app.get("/Tabuada", tabuada);
 
 app.listen(porta, host, () =>{
     console.log("Servidor em execução http://" + host + ":" + porta);
