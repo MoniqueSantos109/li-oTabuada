8123e9ca7aca4938f59a5d3e1e1ba3e5f0511599
monique <moniquefernanda109@gmail.com>
2024-10-23 19:47:18 -0300

    teste

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const app = express();
 function PaginaInicial(requisicao, resposta){
     resposta.send('<h1>Seja bem vindo</h1>');
 }
+function PaginaInicial(numero, sequencia){
+    resposta.send('<h1>Seja bem vindo</h1>');
+    let resultado = '';
+    for (let i = 0; i <= sequencia; i++) {
+        resultado += `<p>${numero} x ${i} = ${numero * i}</p>`;
+    }
+    const numero = parseInt(req.query.tabuada) || 1; // Padrão 1 caso não informado
+    const sequencia = parseInt(req.query.sequencia) || 10; 
+    return resultado;
+}
 
 app.get("/",PaginaInicial);
 
