ad32e3660aa005dd7d2a953776a68967bcb5adf4
monique <moniquefernanda109@gmail.com>
2024-10-23 20:47:30 -0300

    teste final

--- index.js
+++ index.js
@@ -24,11 +24,8 @@ app.get('/', (req, res) => {
 
    
     res.send(`
-        <!DOCTYPE html>
-        <html lang="pt-BR">
+        <html>
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Tabuada do ${numero}</title>
             <style>
                 body {
